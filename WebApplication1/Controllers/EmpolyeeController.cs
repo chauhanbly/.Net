c3a7cd5548bf9c332cbb6983c2d2ee945db41d1f
unknown <rajan.chauhan.bly@gmail.com>
2019-02-14 19:39:16 +0530

    heelooo

---
+++ WebApplication1/Controllers/EmpolyeeController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmpolyeeController : ApiController
+    {
+        private dbModel db = new dbModel();
+
+        // GET: api/Empolyee
+        public IQueryable<Empolyee> GetEmpolyees()
+        {
+            return db.Empolyees;
+        }
+
+        // GET: api/Empolyee/5
+        [ResponseType(typeof(Empolyee))]
+        public IHttpActionResult GetEmpolyee(int id)
+        {
+            Empolyee empolyee = db.Empolyees.Find(id);
+            if (empolyee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(empolyee);
+        }
+
+        // PUT: api/Empolyee/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEmpolyee(int id, Empolyee empolyee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != empolyee.EmployeeId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(empolyee).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmpolyeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Empolyee
+        [ResponseType(typeof(Empolyee))]
+        public IHttpActionResult PostEmpolyee(Empolyee empolyee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Empolyees.Add(empolyee);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = empolyee.EmployeeId }, empolyee);
+        }
+
+        // DELETE: api/Empolyee/5
+        [ResponseType(typeof(Empolyee))]
+        public IHttpActionResult DeleteEmpolyee(int id)
+        {
+            Empolyee empolyee = db.Empolyees.Find(id);
+            if (empolyee == null)
+            {
+                return NotFound();
+            }
+
+            db.Empolyees.Remove(empolyee);
+            db.SaveChanges();
+
+            return Ok(empolyee);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmpolyeeExists(int id)
+        {
+            return db.Empolyees.Count(e => e.EmployeeId == id) > 0;
+        }
+    }
+}
---
+++ WebApplication1/Controllers/LoginDetailController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginDetailController : ApiController
+    {
+        private dbModel db = new dbModel();
+
+        // GET: api/LoginDetail
+        public IQueryable<LoginDetail> GetLoginDetails()
+        {
+            return db.LoginDetails;
+        }
+
+        // GET: api/LoginDetail/5
+        [ResponseType(typeof(LoginDetail))]
+        public IHttpActionResult GetLoginDetail(int id)
+        {
+            LoginDetail loginDetail = db.LoginDetails.Find(id);
+            if (loginDetail == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(loginDetail);
+        }
+
+        // PUT: api/LoginDetail/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutLoginDetail(int id, LoginDetail loginDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != loginDetail.loginid)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(loginDetail).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LoginDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/LoginDetail
+        [ResponseType(typeof(LoginDetail))]
+        public IHttpActionResult PostLoginDetail(LoginDetail loginDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.LoginDetails.Add(loginDetail);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = loginDetail.loginid }, loginDetail);
+        }
+
+        // DELETE: api/LoginDetail/5
+        [ResponseType(typeof(LoginDetail))]
+        public IHttpActionResult DeleteLoginDetail(int id)
+        {
+            LoginDetail loginDetail = db.LoginDetails.Find(id);
+            if (loginDetail == null)
+            {
+                return NotFound();
+            }
+
+            db.LoginDetails.Remove(loginDetail);
+            db.SaveChanges();
+
+            return Ok(loginDetail);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LoginDetailExists(int id)
+        {
+            return db.LoginDetails.Count(e => e.loginid == id) > 0;
+        }
+    }
+}
